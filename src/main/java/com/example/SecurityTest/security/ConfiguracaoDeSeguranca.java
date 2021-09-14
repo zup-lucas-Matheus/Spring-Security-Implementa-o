e9a78da8df0495a65d8bdb8b2084014dd0bbe793
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-14 13:51:57 -0300

    Adicionando implementação do configure HTTP para a liberação de acesso dos endPoints.

--- src/main/java/com/example/SecurityTest/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/com/example/SecurityTest/security/ConfiguracaoDeSeguranca.java
@@ -20,5 +20,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/usuarios").permitAll()
+            .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+
+
 
 }
